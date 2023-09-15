9fb0cff3df7a7aa028b0d9003afabf9b9fc6fbb6
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2023-09-15 20:18:51 +0530

    updates
    updates

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+console.log('');
+console.log('//Munzi Backend//');
+console.log('');
+require('dotenv').config();
+var res = require('dotenv').config();
+const appUtils = require('./lib/appUtils')
+const market = require('./lib/Market/marketDao')
+const MarketUtilities = require('./lib/Market/marketUtilities')
+//Import Config
+const config = require('./lib/config');
+// const socket = require("socket.io");
+
+ config.dbConfig((err) => {
+  if (err) {
+    // logger.error(err, 'exiting the app.');
+
+    console.log({ err });
+    return;
+  }
+
+  // load external modules
+  const express = require('express');
+
+  // init express app
+  const app = express();
+
+  // config express
+  config.expressConfig(app);
+  if (err) return res.json(err);
+
+  // attach the routes to the app
+  require('./lib/routes')(app);
+
+
+const port=2000
+  // start server
+  const server = app.listen(port, () => {
+    console.log(`Express server listening on ${port}`);
+    // logger.info(`Express server listening on ${config.cfg.port}, in ${config.cfg.TAG} mode`);
+  });
+
+ 
+});

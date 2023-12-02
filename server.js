66fd5abbd258f5a231d47a2dfb2dd3058498c7c4
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2023-12-02 19:24:28 +0530

    updates

--- server.js
+++ server.js
@@ -1,59 +1,60 @@
-'use strict';
+"use strict";
 
-console.log('');
-console.log('//API SANDWICH BACKEND//');
-console.log('');
-require('dotenv').config();
-var res = require('dotenv').config();
-const cron = require('node-cron');
-const config = require('./lib/config');
-const { myFunction} = require('./lib/user/scheduler/scheduler');
-const adminDao = require('./lib/user/adminDao');
-const { getAllUserTx } = require('./lib/user/adminService');
-const moment = require('moment-timezone');
+console.log("");
+console.log("//API SANDWICH BACKEND//");
+console.log("");
+require("dotenv").config();
+var res = require("dotenv").config();
+const cron = require("node-cron");
+const config = require("./lib/config");
+const { myFunction } = require("./lib/user/scheduler/scheduler");
+const adminDao = require("./lib/user/adminDao");
+const { getAllUserTx } = require("./lib/user/adminService");
+const moment = require("moment-timezone");
 
-cron.schedule('0 30 18 * * *',async  () => {
+cron.schedule("0 30 18 * * *", async () => {
   // Get the last execution date from the file
   const admin = await adminDao.getUserDetails({
-    emailId: 'samir123@payhub'
+    emailId: "samir123@payhub",
   });
-  const lastExecutionDate = admin.lastExecutionDate
+  const lastExecutionDate = admin.lastExecutionDate;
 
   // Get the current date
-  const currentDate = new Date().toISOString().split('T')[0];
+  const currentDate = new Date().toISOString().split("T")[0];
 
   // Check if the function has not been executed today
   if (lastExecutionDate !== currentDate) {
     // Run your function
     myFunction();
-    console.log('running')
+    console.log("running");
 
     // Update the last execution date in the file
     //fs.writeFileSync(DATE_FILE, currentDate);
-    const update ={
-      lastExecutionDate:currentDate
-    }
-    adminDao.updateProfile({
-      emailId: 'samir123@payhub'
-    },update)
+    const update = {
+      lastExecutionDate: currentDate,
+    };
+    adminDao.updateProfile(
+      {
+        emailId: "samir123@payhub",
+      },
+      update
+    );
   }
 });
 
-cron.schedule('*/20 * * * *', async () => {
-  await adminDao.updateVolumeData('success')
-  await adminDao.getTotalVolume('success')
-  await adminDao.updateGatewayVolumeData()
-
+cron.schedule("*/20 * * * *", async () => {
+  await adminDao.updateVolumeData("success");
+  await adminDao.getTotalVolume("success");
+  await adminDao.updateGatewayVolumeData();
 });
 
-cron.schedule('*/60 * * * *', async () => {
- 
-  await adminDao.updateTotalGatewayBalance()
-
+cron.schedule("*/30 * * * *", async () => {
+  await adminDao.updateTotalGatewayBalance();
+  adminDao.updateBalanceMerchants()
 });
 
 
- config.dbConfig((err) => {
+config.dbConfig((err) => {
   if (err) {
     // logger.error(err, 'exiting the app.');
 
@@ -62,7 +63,7 @@ cron.schedule('*/60 * * * *', async () => {
   }
 
   // load external modules
-  const express = require('express');
+  const express = require("express");
 
   // init express app
   const app = express();
@@ -72,16 +73,13 @@ cron.schedule('*/60 * * * *', async () => {
   if (err) return res.json(err);
 
   // attach the routes to the app
-  require('./lib/routes')(app);
+  require("./lib/routes")(app);
 
-
-  const port = process.env.PORT || 2000;  // start server
+  const port = process.env.PORT || 2000; // start server
   const server = app.listen(port, () => {
     console.log(`Express server listening on ${port}`);
     // logger.info(`Express server listening on ${config.cfg.port}, in ${config.cfg.TAG} mode`);
   });
-
- 
 });
 /**
  * 
